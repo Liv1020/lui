d4680198be4d67d7bb9ab7c2520ee373bbf5d356
Liv <523260513@qq.com>
2015-04-28 11:27:24 +0800

    update

--- Html.php
+++ Html.php
@@ -11,4 +11,27 @@ namespace lui;
 
 class Html extends BaseHtml {
 
+    /**
+     * 删除配置，并获得该配置信息
+     * @param $options
+     * @param $name
+     * @param null $default
+     * @return mixed|null
+     */
+    public function removeOption(&$options,$name,$default = null){
+
+        return ArrayHelper::remove($options,$name,$default);
+    }
+
+    /**
+     * 获取配置信息
+     * @param $options
+     * @param $name
+     * @param null $default
+     * @return mixed
+     */
+    public function getOption($options,$name,$default = null){
+
+        return ArrayHelper::getValue($options,$name,$default);
+    }
 }
